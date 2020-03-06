9c69f74901f5d88c5422a7dddb4873303b309620
gwaygenomics <gregory.way@gmail.com>
2020-03-06 15:16:09 -0500

    shrink figure size

--- 1.profiling-audit/scripts/viz_utils.py
+++ 1.profiling-audit/scripts/viz_utils.py
@@ -25,16 +25,18 @@ def plot_replicate_correlation(
     output_file_base=None,
     output_file_extensions=[".png", ".pdf", ".svg"],
     dpi=300,
-    height=6,
-    width=8,
+    height=3,
+    width=3.5,
 ):
     correlation_gg = (
         gg.ggplot(
             df,
             gg.aes(x="replicate_info", y="pairwise_correlation", fill="replicate_info"),
         )
-        + gg.geom_boxplot(alpha=0.3, outlier_alpha=0)
-        + gg.geom_jitter(shape=".", size=0.5, alpha=0.3)
+        + gg.geom_boxplot(
+            alpha=0.3, outlier_alpha=0, width=0.8, notchwidth=0.25, fatten=1.5
+        )
+        + gg.geom_jitter(shape=".", size=0.001, alpha=0.3, width=0.3, height=0)
         + gg.scale_fill_manual(
             name="Replicate",
             labels={"True": "True", "False": "False"},
@@ -46,9 +48,12 @@ def plot_replicate_correlation(
         + gg.theme_bw()
         + gg.theme(
             subplots_adjust={"wspace": 0.2},
-            axis_text=gg.element_text(size=7),
-            axis_title=gg.element_text(size=9),
-            strip_text=gg.element_text(size=6, color="black"),
+            title=gg.element_text(size=9),
+            axis_text=gg.element_text(size=5),
+            axis_title=gg.element_text(size=8),
+            legend_text=gg.element_text(size=6),
+            legend_title=gg.element_text(size=7),
+            strip_text=gg.element_text(size=4, color="black"),
             strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
         )
     )
@@ -72,8 +77,8 @@ def plot_replicate_density(
     output_file_base=None,
     output_file_extensions=[".png", ".pdf", ".svg"],
     dpi=300,
-    height=4,
-    width=5,
+    height=1.5,
+    width=2,
 ):
     density_gg = (
         gg.ggplot(df, gg.aes(x="pairwise_correlation", fill="replicate_info"))
@@ -87,6 +92,15 @@ def plot_replicate_density(
         + gg.ylab("Density")
         + gg.ggtitle("{}: {}".format(batch, plate))
         + gg.theme_bw()
+        + gg.theme(
+            title=gg.element_text(size=9),
+            axis_text=gg.element_text(size=5),
+            axis_title=gg.element_text(size=8),
+            legend_text=gg.element_text(size=6),
+            legend_title=gg.element_text(size=7),
+            strip_text=gg.element_text(size=4, color="black"),
+            strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
+        )
     )
 
     if output_file_base:
