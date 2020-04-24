6d25a6405e204113841e9606b43b229f5ae745bb
gwaygenomics <gregory.way@gmail.com>
2020-04-24 14:38:09 -0400

    update umap figures

--- 2.describe-data/scripts/nbconverted/3.umap.py
+++ 2.describe-data/scripts/nbconverted/3.umap.py
@@ -55,70 +55,124 @@ def process_umap(data_df):
     return embedding_df
 
 
+# In[5]:
+
+
+save_file_extensions = ['.png']
+
+
 # ## For Combined Batches of Four WT + Resistant Clones
 
-# In[5]:
+# In[6]:
 
 
 # Load and process data
 file = os.path.join("data", "merged", "combined_four_clone_dataset.csv")
-data_df = pd.read_csv(file)
+fourclone_data_df = pd.read_csv(file)
 
-embedding_df = process_umap(data_df)
+embedding_df = process_umap(fourclone_data_df)
 embedding_df.head()
 
 
 # ## Visualize a Series of UMAP Representations
 
-# In[6]:
+# In[7]:
 
 
 umap_resistant_type_gg = (
     gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
     + gg.geom_point(
         gg.aes(fill="Metadata_clone_type", shape="Metadata_treatment"),
-        color='black', alpha=0.8)
+        color='black', alpha=0.6)
     + gg.theme_bw()
     + gg.xlab("UMAP (X)")
     + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Four Clone Dataset - Merged")
     + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
+    + gg.scale_fill_manual(name="Clone Type", values=["#1F8AA5", "#E98831"])
 )
 
 file = os.path.join("figures", "umap", "four_clone_umap_resistant_type")
-for extension in ['.png', '.pdf', '.svg']:
-    umap_resistant_type_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    umap_resistant_type_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
 
 umap_resistant_type_gg
-
-
-# In[7]:
-
-
-umap_batch_gg = (
-    gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
-    + gg.geom_point(gg.aes(fill="Metadata_batch"), color='black', alpha=0.8)
-    + gg.theme_bw()
-    + gg.xlab("UMAP (X)")
-    + gg.ylab("UMAP (Y)")
-)
-
-file = os.path.join("figures", "umap", "four_clone_umap_batch")
-for extension in ['.png', '.pdf', '.svg']:
-    umap_batch_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
-
-umap_batch_gg
 
 
 # In[8]:
 
 
-umap_batch_facet_gg = (
-    gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
-    + gg.geom_point(gg.aes(fill="Metadata_batch"), color='black', alpha=0.8, size=0.8)
+umap_cell_count_gg = (
+    gg.ggplot(
+        embedding_df.rename({"Metadata_cell_count": "Cell Count"}, axis="columns")
+    )
+    + gg.geom_point(
+        gg.aes(x="x", y="y", fill="Cell Count"),
+        color='black', alpha=0.6)
     + gg.theme_bw()
     + gg.xlab("UMAP (X)")
     + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Four Clone Dataset - Merged")
+)
+
+file = os.path.join("figures", "umap", "four_clone_umap_cell_count")
+for extension in save_file_extensions:
+    umap_cell_count_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
+    
+umap_cell_count_gg
+
+
+# In[9]:
+
+
+umap_batch_gg = (
+    gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
+    + gg.geom_point(
+        gg.aes(fill="Metadata_batch", shape="Metadata_treatment"),
+        color='black',
+        alpha=0.6
+    )
+    + gg.theme_bw()
+    + gg.xlab("UMAP (X)")
+    + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Four Clone Dataset - Merged")
+    + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
+    + gg.scale_fill_manual(
+        name="Batch",
+        values=["#1b9e77", "#d95f02", "#7570b3"],
+        labels=['Batch 5', "Batch 6", "Batch 7"]
+    )
+)
+
+file = os.path.join("figures", "umap", "four_clone_umap_batch")
+for extension in save_file_extensions:
+    umap_batch_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
+
+umap_batch_gg
+
+
+# In[10]:
+
+
+umap_batch_facet_gg = (
+    gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
+     + gg.geom_point(
+        gg.aes(fill="Metadata_batch", shape="Metadata_treatment"),
+        color='black',
+        alpha=0.8,
+        size=0.6
+    )
+    + gg.theme_bw()
+    + gg.xlab("UMAP (X)")
+    + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Four Clone Dataset - Merged")
     + gg.facet_wrap("~Metadata_plate_ID")
+    + gg.scale_fill_manual(
+        name="Batch",
+        values=["#1b9e77", "#d95f02", "#7570b3"],
+        labels=['Batch 5', "Batch 6", "Batch 7"]
+    )
+    + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
     + gg.theme(
         strip_text=gg.element_text(size=6, color="black"),
         strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
@@ -126,93 +180,99 @@ umap_batch_facet_gg = (
 )
     
 file = os.path.join("figures", "umap", "four_clone_umap_plate_facet")
-for extension in ['.png', '.pdf', '.svg']:
-    umap_batch_facet_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    umap_batch_facet_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
 
 umap_batch_facet_gg
 
 
-# In[9]:
+# In[11]:
 
 
 # Visualize UMAP results
 clone_facet_gg = (
-    gg.ggplot(embedding_df,
-              gg.aes('x', 'y', color='factor(Metadata_Plate)', shape="Metadata_treatment"))
-    + gg.geom_point()
+    gg.ggplot(embedding_df, gg.aes('x', 'y'))
+    + gg.geom_point(
+        gg.aes(fill='factor(Metadata_Plate)', shape="Metadata_treatment", alpha=0.6)
+    )
     + gg.theme_bw()
     + gg.xlab("UMAP X")
     + gg.ylab("UMAP Y")
     + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
-    + gg.scale_color_discrete(name="Plate")
+    + gg.scale_fill_discrete(name="Plate")
     + gg.facet_wrap("~Metadata_clone_number")
-    + gg.ggtitle("Cell Painting Four Clones")
+    + gg.ggtitle("Four Clone Dataset - Merged")
     + gg.theme(
         legend_key=gg.element_rect(color="black", fill = "white"),
+        strip_text=gg.element_text(size=6, color="black"),
         strip_background=gg.element_rect(colour="black", fill="#fdfff4")
     )
 )
     
 file = os.path.join("figures", "umap", "four_clone_umap_facet_clone_sample")
-for extension in ['.png', '.pdf', '.svg']:
-    clone_facet_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    clone_facet_gg.save(filename='{}{}'.format(file, extension), height=4, width=4.5, dpi=400)
 
 clone_facet_gg
 
 
-# In[10]:
+# In[12]:
 
 
 umap_well_embedding_gg = (
     gg.ggplot(embedding_df, gg.aes(x="x", y="y"))
     + gg.geom_point(
         gg.aes(fill="Metadata_clone_number", shape="Metadata_treatment"),
-        color='black', alpha=0.8
+        color='black', alpha=0.6
     )
     + gg.theme_bw()
     + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
+    + gg.scale_fill_discrete(name="Clone")
     + gg.xlab("UMAP (X)")
     + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Four Clone Dataset - Merged")
 )
 
 file = os.path.join("figures", "umap", "four_clone_umap_clone_sample")
-for extension in ['.png', '.pdf', '.svg']:
-    umap_well_embedding_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    umap_well_embedding_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
 
 umap_well_embedding_gg
 
 
 # ## For Clone A and E Data
 
-# In[11]:
+# In[13]:
 
 
 # Load and process data
 file = os.path.join("data", "merged", "combined_cloneAcloneE_dataset.csv")
 cloneAE_data_df = pd.read_csv(file)
 
-embedding_df = process_umap(cloneAE_data_df)
-embedding_df.head()
+embedding_cloneAE_df = process_umap(cloneAE_data_df)
+embedding_cloneAE_df.head()
 
 
-# In[12]:
+# In[14]:
 
 
 # Visualize UMAP results
 clone_ae_umap_gg = (
-    gg.ggplot(embedding_df,
-              gg.aes('x', 'y',
-                     shape="Metadata_Plate", 
-                     size='factor(Metadata_Dosage)',
-                     color="Metadata_CellLine"))
-    + gg.geom_point()
+    gg.ggplot(embedding_cloneAE_df)
+    + gg.geom_point(
+        gg.aes('x', 'y',
+               shape="Metadata_Plate", 
+               size='factor(Metadata_Dosage)',
+               color="Metadata_CellLine"),
+        alpha=0.8
+    )
     + gg.theme_bw()
     + gg.scale_shape_manual(name="Plate", values=[".", "+"])
     + gg.scale_color_discrete(name="Clone")
-    + gg.scale_size_discrete(name="Dosage")
+    + gg.scale_size_manual(name="Dosage", values=[1, 3, 5, 7])
     + gg.xlab("UMAP X")
     + gg.ylab("UMAP Y")
-    + gg.ggtitle("Cell Painting Clone A and E")
+    + gg.ggtitle("Clone A and E - Merged")
     + gg.theme(
         legend_key=gg.element_rect(color="black", fill = "white"),
         strip_background=gg.element_rect(colour="black", fill="#fdfff4")
@@ -220,21 +280,44 @@ clone_ae_umap_gg = (
 )
 
 file = os.path.join("figures", "umap", "cloneAE_umap")
-for extension in ['.png', '.pdf', '.svg']:
-    clone_ae_umap_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    clone_ae_umap_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
 
 clone_ae_umap_gg
 
 
+# In[15]:
+
+
+clone_ae_umap_cell_count_gg = (
+    gg.ggplot(
+        embedding_cloneAE_df.rename({"Metadata_cell_count": "Cell Count"}, axis="columns")
+    )
+    + gg.geom_point(
+        gg.aes(x="x", y="y", fill="Cell Count"),
+        color='black', alpha=0.6)
+    + gg.theme_bw()
+    + gg.xlab("UMAP (X)")
+    + gg.ylab("UMAP (Y)")
+    + gg.ggtitle("Clone A and E - Merged")
+)
+
+file = os.path.join("figures", "umap", "cloneAE_umap_cell_count")
+for extension in save_file_extensions:
+    clone_ae_umap_cell_count_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
+    
+clone_ae_umap_cell_count_gg
+
+
 # ## Merged Data
 
-# In[13]:
+# In[16]:
 
 
 drop_cols = ["Metadata_plate_ID", "Metadata_plate_filename"]
 
-data_recode_df = (
-    data_df
+fourclone_data_recode_df = (
+    fourclone_data_df
     .drop(drop_cols, axis="columns")
     .rename(
         {
@@ -253,48 +336,50 @@ cloneAE_data_recode_df = (
 cloneAE_data_recode_df.loc[cloneAE_data_recode_df.Metadata_Dosage == 0, "Metadata_treatment"] = "DMSO"
 
 
-# In[14]:
+# In[17]:
 
 
-combined_df = pd.concat([data_recode_df, cloneAE_data_recode_df], sort=True).reset_index(drop=True)
+combined_df = pd.concat([fourclone_data_recode_df, cloneAE_data_recode_df], sort=True).reset_index(drop=True)
 combined_df = feature_select(combined_df, operation="drop_na_columns")
 
 print(combined_df.shape)
 combined_df.head()
 
 
-# In[15]:
+# In[18]:
 
 
-embedding_df = process_umap(combined_df)
-embedding_df.head()
+embedding_combined_df = process_umap(combined_df)
+embedding_combined_df.head()
 
 
-# In[16]:
+# In[19]:
 
 
 # Visualize UMAP results
 merged_umap_gg = (
-    gg.ggplot(
-        embedding_df,
+    gg.ggplot(embedding_combined_df)
+    + gg.geom_point(
         gg.aes(
             'x', 'y',
             shape="Metadata_treatment",
-            color='Metadata_Dataset'
-        )
+            fill='Metadata_Dataset'
+        ),
+        size=1,
+        alpha=0.4
     )
-    + gg.geom_point()
     + gg.theme_bw()
     + gg.xlab("x")
     + gg.ylab("y")
-    + gg.scale_shape_manual(name="Treatment", values=[".", "+"])
+    + gg.scale_shape_manual(name="Treatment", values=["^", "+"])
+    + gg.scale_fill_manual(name="Dataset", values=["#F0C13E", "#BD4135"])
     + gg.ggtitle("Cell Painting Merged (UMAP)")
     + gg.theme(legend_key=gg.element_rect(color="black", fill = "white"))
 )
 
 file = os.path.join("figures", "umap", "clone_compare_batch_effect")
-for extension in ['.png', '.pdf', '.svg']:
-    merged_umap_gg.save(filename='{}{}'.format(file, extension), height=3, width=4, dpi=400)
+for extension in save_file_extensions:
+    merged_umap_gg.save(filename='{}{}'.format(file, extension), height=3, width=3.5, dpi=400)
 
 merged_umap_gg
 
