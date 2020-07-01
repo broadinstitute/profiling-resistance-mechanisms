1e3ee399ba082ad4460d8bf63068786801b943a5
gwaygenomics <gregory.way@gmail.com>
2020-07-01 13:57:24 -0400

    remove train test split option from process data

--- 4.single-cell/utils/single_cell_utils.py
+++ 4.single-cell/utils/single_cell_utils.py
@@ -51,10 +51,7 @@ def process_data(
     imagenumber,
     image_df,
     feature_filter,
-    scaler_method="standard",
     seed=123,
-    test_split_prop=0.15,
-    normalize=False,
 ):
     # Load compartments
     cell_df = load_compartment_site("cells", connection, imagenumber)
@@ -83,19 +80,7 @@ def process_data(
         merged_df, on=["TableNumber", "ImageNumber"], how="right"
     )
 
-    # Normalize data
-    if normalize:
-        merged_df = normalize_sc(merged_df, scaler_method=scaler_method)
-
-    # Split training and testing
-    train_df, test_df = train_test_split(
-        merged_df, test_size=test_split_prop, random_state=seed
-    )
-
-    train_df = train_df.reset_index(drop=True)
-    test_df = test_df.reset_index(drop=True)
-
-    return train_df, test_df
+    return merged_df
 
 
 def process_sites(
@@ -103,11 +88,9 @@ def process_sites(
     imagenumbers,
     image_df,
     feature_filter,
-    scaler_method="standard",
     seed=123,
-    test_split_prop=0.15,
-    normalize_before_split=True,
-    normalize_after_split=False
+    scaler_method="standard",
+    normalize=True
 ):
     if normalize_before_split:
         assert not normalize_after_split, "Do not normalize twice!"
@@ -115,27 +98,21 @@ def process_sites(
     if normalize_after_split:
         assert not normalize_before_split, "Do not normalize twice!"
 
-    train_df = {}
-    test_df = {}
-
+    data_df = {}
     for imagenumber in imagenumbers:
         print(f"ImageNumber: {imagenumber}")
-        train_df[imagenumber], test_df[imagenumber] = process_data(
+        data_df[imagenumber] = process_data(
             connection=connection,
             imagenumber=imagenumber,
             image_df=image_df,
             feature_filter=feature_filter,
             scaler_method=scaler_method,
-            seed=seed,
-            test_split_prop=test_split_prop,
-            normalize=normalize_before_split,
+            seed=seed
         )
 
-    train_df = pd.concat(train_df).reset_index(drop=True)
-    test_df = pd.concat(test_df).reset_index(drop=True)
+    data_df = pd.concat(data_df).reset_index(drop=True)
 
-    if normalize_after_split:
-        train_df = normalize_sc(train_df, scaler_method=scaler_method)
-        test_df = normalize_sc(test_df, scaler_method=scaler_method)
+    if normalize:
+        data_df = normalize_sc(data_df, scaler_method=scaler_method)
 
-    return train_df, test_df
+    return data_df
