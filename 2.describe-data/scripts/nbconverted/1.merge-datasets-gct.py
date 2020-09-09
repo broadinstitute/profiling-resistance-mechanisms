3b096d01e043dcdb902c0040d7bc38c4ddd4b3cd
gwaygenomics <gregory.way@gmail.com>
2020-09-09 17:11:11 -0400

    rename nbconverted

--- 2.describe-data/scripts/nbconverted/1.merge-datasets-gct.py
+++ 2.describe-data/scripts/nbconverted/1.merge-datasets-gct.py
@@ -13,13 +13,8 @@
 import os
 import pandas as pd
 
-from pycytominer import (
-    feature_select,
-    write_gct
-)
-
-from pycytominer.consensus import modz
-from pycytominer.cyto_utils import infer_cp_features
+from pycytominer import feature_select
+from pycytominer.cyto_utils import infer_cp_features, write_gct
 
 from scripts.processing_utils import load_data
 
@@ -32,9 +27,10 @@ feature_select_ops = [
     "variance_threshold",
     "correlation_threshold",
     "drop_na_columns",
-    "blacklist",
+    "blocklist",
     "drop_outliers"
 ]
+
 gct_dir = os.path.join("data", "gct_files")
 profile_dir = os.path.join("..", "0.generate-profiles", "profiles")
 cell_count_dir = os.path.join("..", "0.generate-profiles", "cell_counts")
@@ -42,7 +38,8 @@ output_dir = os.path.join("data", "merged")
 
 suffix = "normalized.csv.gz"
 
-batches = [x for x in os.listdir(profile_dir) if x != ".DS_Store"]
+# Ignore 40X batch
+batches = [x for x in os.listdir(profile_dir) if x not in [".DS_Store", "2019_02_15_Batch1_40X"]]
 batches
 
 
@@ -65,18 +62,18 @@ for batch in batches:
         profile_dir=profile_dir,
         combine_dfs=True,
         add_cell_count=True,
+        harmonize_cols=True,
         cell_count_dir=cell_count_dir
     )
-    
+
     # Save normalized and non-feature selected data
     profile_batches[batch] = df
     
-    # Apply feature selection again - this is particularly important for batches
-    # with multiple plates
-    df = feature_select(df, operation=feature_select_ops)
-    
+    # Apply feature selection
+    feature_select_df = feature_select(df, operation=feature_select_ops)
+        
     # Write the dataframe as a gct file for input into Morpheus
-    write_gct(profiles=df, output_file=output_gct_file)
+    write_gct(profiles=feature_select_df, output_file=output_gct_file)
 
 
 # ## Merge Profiles Together and Output
@@ -85,6 +82,9 @@ for batch in batches:
 
 
 all_profiles_df = pd.concat(profile_batches.values(), sort=True).reset_index(drop=True)
+
+all_profiles_df = all_profiles_df.assign(Metadata_clone_type="resistant")
+all_profiles_df.loc[all_profiles_df.Metadata_clone_number.str.contains("WT"), "Metadata_clone_type"] = "wildtype"
 
 meta_features = infer_cp_features(all_profiles_df, metadata=True)
 cp_cols = infer_cp_features(all_profiles_df, metadata=False)
@@ -100,6 +100,8 @@ all_profiles_df.head()
 
 all_profiles_df = feature_select(all_profiles_df, operation=feature_select_ops)
 
+all_profiles_df = all_profiles_df.drop(["Metadata_plate_ID", "Metadata_plate_filename"], axis="columns")
+
 print(all_profiles_df.shape)
 all_profiles_df.head()
 
@@ -110,61 +112,31 @@ all_profiles_df.head()
 output_file = os.path.join(output_dir, "all_merged_profiles.csv.gz")
 all_profiles_df.to_csv(output_file, index=False, compression="gzip")
 
+output_gct_file = os.path.join(gct_dir, "all_merged_profiles.gct")
+write_gct(profiles=all_profiles_df, output_file=output_gct_file)
 
-# ## Generate Consensus Signatures
+
+# ## Collapse replicates into consensus profiles
 
 # In[7]:
 
 
-consensus_data = {}
-for batch in profile_batches:
-    meta_features = infer_cp_features(profile_batches[batch], metadata=True)
-    meta_features = [x for x in meta_features if "well" not in x.lower()]
-    meta_features = [x for x in meta_features if "site" not in x.lower()]
-    
-    consensus_df = (
-        profile_batches[batch]
-        .groupby(meta_features)
-        .median()
-        .reset_index(drop=False)
-    )
-    
-    consensus_data[batch] = consensus_df.reset_index()
+median_consensus_df = (
+    all_profiles_df.groupby(["Metadata_clone_number", "Metadata_treatment"])
+    .median()
+    .reset_index()
+)
+
+print(median_consensus_df.shape)
+median_consensus_df.head()
 
 
 # In[8]:
 
 
-full_consensus_df = (
-    pd.concat(consensus_data.values(), sort=True)
-    .reset_index(drop=True)
-)
+output_file = os.path.join(output_dir, "consensus_profiles.csv.gz")
+median_consensus_df.to_csv(output_file, index=False, compression="gzip")
 
-meta_features = infer_cp_features(full_consensus_df, metadata=True)
-cp_cols = infer_cp_features(full_consensus_df, metadata=False)
-
-full_consensus_df = (
-    full_consensus_df
-    .reindex(meta_features + cp_cols, axis="columns")
-    .drop("Metadata_cell_count", axis="columns")
-)
-
-print(full_consensus_df.shape)
-full_consensus_df.head()
-
-
-# In[9]:
-
-
-consensus_df = feature_select(full_consensus_df, operation=feature_select_ops)
-
-print(consensus_df.shape)
-consensus_df.head()
-
-
-# In[10]:
-
-
-output_gct_file = os.path.join(gct_dir, "consensus_feature_select.gct")
-write_gct(profiles=consensus_df, output_file=output_gct_file)
+output_gct_file = os.path.join(gct_dir, "consensus_profiles.gct")
+write_gct(profiles=median_consensus_df, output_file=output_gct_file)
 
